043fc140f57aa9539d411f6dac56c847ff7febe5
gid gud <daniluv0902@gmail.com>
2024-12-04 16:12:16 -0800

    улучшение

--- src/login.tsx
+++ src/login.tsx
@@ -1,18 +1,19 @@
 import {useState, useEffect} from 'react'
 import {useSelector, useDispatch} from 'react-redux'
 import './styles/intro.css'
+import './styles/globals.css'
 
 function Login() {
     return (
         <> 
-            <div className='container flex-row items-center align-middle space-y-10'>
+            <div className='container flex flex-col items-center align-middle space-y-10'>
                 <div className='intro flex-row items-center row-start-1'>
-                    <h2> Добро пожаловать! </h2>
-                    <span> Введите <b>логин</b> и <b>пароль</b> </span>
+                    <h2 className='text-3xl p-6 m-1'> Добро пожаловать! </h2>
+                    <span className='text-base'> Введите <b>логин</b> и <b>пароль</b> </span>
                 </div>
-                <div className='input flex flex-row items-start'>
-                    <input type='text' className='flex p-2 border border-gray-300 rounded' placeholder='Логин' />
-                    <input type='password' className= 'flex p-2 border border-gray-300 rounded' placeholder='Пароль' />
+                <div className='input flex flex-col items-start w-1/2'>
+                    <input type='text' className='login p-2 border border-gray-300 rounded w-full' placeholder='Логин' />
+                    <input type='password' className= 'passwrd p-2 border border-gray-300 rounded w-full' placeholder='Пароль' />
                 </div>
             </div>
         </>
