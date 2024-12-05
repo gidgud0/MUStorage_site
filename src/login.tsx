e04f43de8dad104d3b0a48621cea1dbe83d523a6
gid gud <daniluv0902@gmail.com>
2024-12-05 15:29:53 -0800

    обнова бг и добавление 2 гиперссылок (нерабочие)

--- src/login.tsx
+++ src/login.tsx
@@ -8,7 +8,7 @@ function Login() {
         <> 
             <div className='container flex flex-col items-center align-middle space-y-10'>
                 <div className='intro flex-row items-center row-start-1'>
-                    <h2 className='text-3xl p-6 m-1'> Добро пожаловать! </h2>
+                    <h2 className='text-3xl p-6 m-1'> Добро пожаловать в MUStorage! </h2>
                     <span className='text-base'> Введите <b>логин</b> и <b>пароль</b> </span>
                 </div>
                 <div className='input flex flex-col items-start w-1/2'>
@@ -16,9 +16,16 @@ function Login() {
                     type='text' 
                     className='login p-2 border border-gray-300 rounded w-full' 
                     placeholder='Логин'
-                     
                     />
                     <input type='password' className= 'passwrd p-2 border border-gray-300 rounded w-full' placeholder='Пароль' />
+                </div>
+                <div className='hl_container flex flex-row justify-start gap-20'>
+                    <div className='fp'>
+                        <a href='#'> Забыли пароль? </a>
+                    </div>
+                    <div className='reg'>
+                        <a href='#'> Зарегистрироваться </a>
+                    </div>
                 </div>
             </div>
         </>
