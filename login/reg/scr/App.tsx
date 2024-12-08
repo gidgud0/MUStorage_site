a44dba07749f17c9d3a21efcc13b12be10912f77
gid gud <daniluv0902@gmail.com>
2024-12-08 14:53:09 -0800

    Исправление багов

--- login/reg/scr/App.tsx
+++ login/reg/scr/App.tsx
@@ -1,7 +1,16 @@
 import { useState } from 'react'
 import './styles/App.css'
+import React from 'react'
 
 function App() {
+    return (
+    <>
+        <div className='container'>
+            
+        </div>
+    </> 
+    )
+
 }
 
 export default App
